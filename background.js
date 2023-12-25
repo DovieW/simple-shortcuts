142596a0fc509545f5915a4aeb6bca5b00b03f3f
Dovie Weinstock <DovieWeinstock@gmail.com>
2023-12-24 23:50:21 -0500

    work on highlighted tabs

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
 chrome.commands.onCommand.addListener((command, tab) => {
   if (command === 'duplicate-tab') {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.tabs.create({ url: tabs[0].url, index: tabs[0].index + 1 });
+    chrome.tabs.query({ highlighted: true, currentWindow: true }, (tabs) => {
+      tabs.forEach(tab => {
+        chrome.tabs.create({ url: tab.url, index: tab.index + 1 });
+      });
     });
   } else if (command === 'pin-tab') {
     chrome.tabs.query({ highlighted: true, currentWindow: true }, (tabs) => {
@@ -10,11 +12,14 @@ chrome.commands.onCommand.addListener((command, tab) => {
       });
     });
   } else if (command === 'go-incognito') {
-    chrome.windows.create({ url: tab.url, incognito: true, focused: true, state: 'maximized' });
+    chrome.tabs.query({ highlighted: true, currentWindow: true }, (tabs) => {
+      const urls = tabs.map(tab => tab.url);
+      if(urls.length > 0) {
+        chrome.windows.create({ url: urls, incognito: true, focused: true, state: 'maximized' });
+      }
+    });
   } else if (command === 'open-tab-near') {
-    // Create a new tab near the current one
     chrome.tabs.create({ index: tab.index + 1 }, (newTab) => {
-      // If the current tab is part of a group, add the new tab to the same group
       if (tab.groupId > 0) {
         chrome.tabs.group({ groupId: tab.groupId, tabIds: newTab.id });
       }
