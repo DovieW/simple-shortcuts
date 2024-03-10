3b02a6e44d2a0dcfc82e376090e9ed1a6c854888
Dovie Weinstock <DovieWeinstock@gmail.com>
2024-03-10 13:03:38 -0400

    fix duplicating pinned tabs

--- background.js
+++ background.js
@@ -1,12 +1,16 @@
 chrome.commands.onCommand.addListener((command, tab) => {
   if (command === 'duplicate-tab') {
-    chrome.tabs.query({ highlighted: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.query({ active: true }, (tabs) => {
       tabs.forEach(tab => {
-        chrome.tabs.create({ url: tab.url, index: tab.index + 1 }, (newTab) => {
-          if (tab.groupId > 0) {
-            chrome.tabs.group({ groupId: tab.groupId, tabIds: newTab.id });
-          }
-        });
+        if (tab.pinned) {
+          chrome.tabs.create({ url: tab.url, index: tab.index + 1, active: true, pinned: true });
+        } else {
+          chrome.tabs.create({ url: tab.url, index: tab.index + 1, active: true }, (newTab) => {
+            if (tab.groupId > 0) {
+              chrome.tabs.group({ groupId: tab.groupId, tabIds: newTab.id });
+            }
+          });
+        }
       });
     });
   } else if (command === 'pin-tab') {
